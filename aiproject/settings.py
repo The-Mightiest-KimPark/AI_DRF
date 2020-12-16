231bd4e0bea6e1e6f901ebcc8c423ebc26378287
sunyeongchoi <sn0716@naver.com>
2020-12-16 21:59:05 +0900

    restframework installed app add

--- aiproject/settings.py
+++ aiproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ai.apps.AiConfig'
+    'ai.apps.AiConfig',
+    'rest_framework'
 ]
 
 REST_FRAMEWORK = {
