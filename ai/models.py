9cb24d7053bfc4b29e949e0f5fdb203944db4f5c
sunyeongchoi <sn0716@naver.com>
2020-12-17 13:19:35 +0900

    test

--- ai/models.py
+++ ai/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import JSONField
+# from django.contrib.postgres.fields import JSONField
 
 
 
