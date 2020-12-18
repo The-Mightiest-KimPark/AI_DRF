f7c25e08edee5481f987ca3f574ec184b296b6d7
Keunung Park <pku928@naver.com>
2020-12-18 16:17:57 +0900

    hyperparameter 변경

--- ai/load.py
+++ ai/load.py
@@ -4,9 +4,11 @@ from ai.my_yolo import YOLO
 class pre_yolo(AppConfig):
     name = 'my_yolo_app'
 
-    print('hello')
     model_path = 'ai/000/trained_weights_final.h5'
     class_path = 'ai/_classes.txt'
-    yolo = YOLO(model_path=model_path, classes_path=class_path)
-    print(yolo)
+    score = 0.5
+    iou = 0.3
+    yolo = YOLO(model_path=model_path, classes_path=class_path, score=score, iou=iou)
+    print('YOLO가 실행중입니다.')
+
 
